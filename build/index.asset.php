f50d633254c8e715525bd653c742917757db58e5
coder-karen <coder-karen@users.noreply.github.com>
2022-04-11 14:46:01 +0000

    Assistant: Add timed discount card (#23562)
    Co-authored-by: Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
    Co-authored-by: jboland88 <18016357+jboland88@users.noreply.github.com>
    Co-authored-by: Karen Attfield <karenlattfield@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2149193504

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '480033531ba698818b711be78e7e52d5');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '67d949483304f57528763bd0145488af');
