d26ba3994dab5e5451f1754a471031e61922e380
leogermani <leogermani@users.noreply.github.com>
2022-04-12 20:54:46 +0000

    Connection JS package: new action to refresh the list of connected Plugins - apply it to My Jetpack (#23358)
    * creating and consuming resolver attempt
    
    * connecetedPlugins in the connection initial state
    
    * refresh list after activating a product (WIP)
    
    * enable thunks and last adjustments
    
    * changelogs
    
    * bumping versions
    
    * removed unused action
    
    * Linting
    
    * always refresh the plugins list
    
    * remove unnecessary function
    
    * dont track page view on component re-render
    
    * use registryControl to dispatch action
    
    * FIx proptypes declaration
    
    * [not verified] Revert "use registryControl to dispatch action"
    
    This reverts commit 72e171307152633ba787ebbe03457faab0b6222c.
    
    * use registry instead of global dispatcher
    
    * [not verified] Revert "dont track page view on component re-render"
    
    This reverts commit db309a32d8b9ef3fcf46672f13bf54ba8bc4fff4.
    
    * dont track event after refreshig connectedPugins
    
    * Linting
    
    * bumping versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2157476686

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '67e4a52cf6c827c83c42015ca919c525');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '54621baff4d1b6b5e6201b5ffe93aa6b');
