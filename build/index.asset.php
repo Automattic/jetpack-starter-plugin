18c16f2bf1d47237bdc479d1b2665a651d68ca94
kraftbj <kraftbj@users.noreply.github.com>
2022-04-19 18:48:29 +0000

    Add logic to fetch intro offers (#23857)
    * Add logic to fetch intro offers
    
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2191501285

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7ad58f27409745dfca951771e4aabf04');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8baa1b05d1f70a5b18809677cf3e4802');
