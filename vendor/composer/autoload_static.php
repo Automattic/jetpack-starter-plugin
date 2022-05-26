6e61a0d39b5ee88633f6521a2338cf617f2ac8f8
danielpost <danielpost@users.noreply.github.com>
2022-05-26 09:00:14 +0000

    My Jetpack: Add Social card (#24432)
    * Add Social to my jetpack.
    
    * changelog
    
    * Added svg icon
    
    * Update projects/packages/my-jetpack/src/products/class-social.php
    
    Co-authored-by: Daniel Post <daniel@danielpost.com>
    
    * Update projects/packages/my-jetpack/src/products/class-social.php
    
    Co-authored-by: Daniel Post <daniel@danielpost.com>
    
    * Swapped to using the updated icon
    
    * Remove unnecessary changelog.
    
    * Change module name to make Social the hybrid product of publicize
    
    * Updated project versions
    
    * adds Social interstitial page
    
    * Resolved lock file issues.
    
    * changelog
    
    * Composer issues
    
    * Updated function doc comments.
    
    * fixed composer.lock file issues
    
    * resolved composer.json and composer.lock issues.
    
    * Fix changelogger validity
    
    * Fix manage url when plugin is not there
    
    Co-authored-by: Daniel Post <daniel@danielpost.com>
    Co-authored-by: Paul Bunkham <paul@dobit.co.uk>
    Co-authored-by: Leo Germani <leogermani@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2389643094

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -86,6 +86,7 @@ class ComposerStaticInitc4802e05bbcf59fd3b6350e8d3e5482c_starter_pluginⓥ0_1_0_
         'Automattic\\Jetpack\\My_Jetpack\\Products\\Search' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-search.php',
         'Automattic\\Jetpack\\My_Jetpack\\Products\\Search_Stats' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-search-stats.php',
         'Automattic\\Jetpack\\My_Jetpack\\Products\\Security' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-security.php',
+        'Automattic\\Jetpack\\My_Jetpack\\Products\\Social' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-social.php',
         'Automattic\\Jetpack\\My_Jetpack\\Products\\Videopress' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/products/class-videopress.php',
         'Automattic\\Jetpack\\My_Jetpack\\REST_Products' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-rest-products.php',
         'Automattic\\Jetpack\\My_Jetpack\\REST_Purchases' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-my-jetpack/src/class-rest-purchases.php',
