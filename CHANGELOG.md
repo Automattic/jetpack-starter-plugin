dd7301bb892c3e4921c2559d03f70a40cb91a940
anomiex <anomiex@users.noreply.github.com>
2022-06-01 16:12:19 +0000

    Add `import/order` eslint rule (#24601)
    This is the same rule used in Calypso; see p4TIVU-9O9-p2.
    
    Unfortunately we have a ton of stuff needing to be reordered to make the
    new rule pass. The reordering should usually be safe. The exception
    would be if the code involved has side effects and those side effects
    need to happen in a particular order.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2422818833

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@ This is an alpha version! The changes listed here are not final.
 - Configure Sync with the minimal amount of data
 - Janitorial: require a more recent version of WordPress now that WP 6.0 is coming out.
 - Remove use of `pnpx` in preparation for pnpm 7.0.
+- Reorder JS imports for `import/order` eslint rule.
 - Updated package dependencies.
 - Update package.json metadata.
 
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'a04e5e8ec9340ae1488d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7961d4e436313dc358aa');
