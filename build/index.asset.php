5ea4c8c271a912a6640ace07226bde3940dc7c92
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-06-15 11:59:21 +0000

    JS Components: Convert JetpackLogo component to TypeScript (#24558)
    * Convert JetpackLogo to functional component
    
    * Convert JetpackLogo to TypeScript
    
    * Convert JetpackLogo tests to TypeScript and get rid of enzyme
    
    * Add @storybook/react as dev dependency for types
    
    * Convert JetpackLogo stories to TypeScript
    
    * Extract types to types.ts file
    
    * Update path in consumer component
    
    * Add changelog
    
    * Update index.tsx
    
    * Fix version
    
    * Fix version
    
    * Fix lints
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2501960682

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '0a52dd01f001282a4281');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '339e910fdba41fcbe947');
