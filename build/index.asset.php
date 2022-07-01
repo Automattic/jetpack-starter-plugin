37f02d44ecca47354e76a16709745aa9018d00c7
renatoagds <renatoagds@users.noreply.github.com>
2022-07-01 14:07:32 +0000

    AutomatticBylineLogo | JetpackLogo: Changing `title` tag to `desc` (#24935)
    * AutomatticBylineLogo: Change title to desc
    
    * changelog
    
    * Update projects/js-packages/components/changelog/fix-inline-logo-description
    
    * JetpackLogo: Change title tag to desc
    
    * Changelog: Update description
    
    Co-authored-by: Steve D <33553323+sdixon194@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2597062899

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '21ffcfeccd6a873815a2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '624e667de5a5801d7003');
