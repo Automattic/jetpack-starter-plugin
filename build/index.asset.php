f36fc34cc3759f45fe0a92203ebaa420e81dea7f
anomiex <anomiex@users.noreply.github.com>
2022-07-01 14:31:53 +0000

    Update babel monorepo to v7.18.6 (#24924)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2597204698

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '624e667de5a5801d7003');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '139df50d585acb697bbf');
