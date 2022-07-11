c39b0b9356a6111fbf6f3c21b3992a514828709e
anomiex <anomiex@users.noreply.github.com>
2022-07-11 17:49:32 +0000

    Lock file maintenance (#25048)
    * Lock file maintenance.
    * Bump direct `@types/react` deps so we only have one version installed.
      Seems safer that way.
    * Avoid `@octokit/plugin-paginate-rest` v2.21.2 peer dep breakage.
    * Avoid annoying flip-flopping of sub-dep peer deps of `localtunnel`.
    * Multiple versions of `@testing-library/dom` break tests, so update
      direct deps too.
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2651336440

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '139df50d585acb697bbf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'ecce8fa97b26c4349e0d');
