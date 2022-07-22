e26f79378de01df26897b538a1ea1c6a3d773132
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-22 15:32:00 +0000

    JS Components: Convert PricingCard component to TypeScript (#24906)
    * Convert PricingCard component to TypeScript
    
    * Convert PricingCard unit tests to TypeScript
    
    * Fix react/jsx runtime for jest
    
    * Convert PricingCard story to TypeScript
    
    * Fix @jest/globals dependency for TypeScript files
    
    * Add changelog
    
    * fix version
    
    * Fix import
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2719512269

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'ecce8fa97b26c4349e0d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '0d6d2a8b806752f03de7');
