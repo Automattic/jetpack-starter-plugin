d7a5630c20724a71d01fc325e68da9f67f0d051a
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-01 13:53:05 +0000

    JS Components: Convert JetpackFooter component to TypeScript (#25295)
    * [not verified] Convert JetpackFooter component to TypeScript
    
    * [not verified] Convert JetpackFooter stories to TypeScript
    
    * [not verified] Convert JetpackFooter tests to TypeScript
    
    * [not verified] Add more test cases
    
    * [not verified] Fix story source not loading
    
    * Add changelog
    
    * E2E tests expect React to be in scope :shrug:
    
    * Fix version
    
    * Add default controls for story
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2775229286

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8ce06651ba48c875b49e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8f72ddc038f0a7b4ff76');
