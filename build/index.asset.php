8945de486736c879716f601d42f012b29348d2a6
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-05 18:52:15 +0000

    JS Components: Convert admin section (AdminSection, AdminSectionHero) components to TypeScript (#25360)
    * Create base shared types
    
    * Convert basic AdminSection to TypeScript
    
    * Convert AdminSection hero to TypeScript
    
    * Convert AdminSection stories to TypeScript
    
    * Fix story source not loading
    
    * Add changelog
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2805549051

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'e05a1e625f8cdf70baab');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '3de54aab54f13ecbd92b');
