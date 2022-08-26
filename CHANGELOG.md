ccb5c5fe072d0574a93f16c29fd12050cb6776bc
renatoagds <renatoagds@users.noreply.github.com>
2022-08-26 16:03:18 +0000

    Starter Plugin: Introduce ThemeProvider to wrap entire application (#25870)
    * Use ThemeProvider when rendering Starter Plugin AdminPage
    
    * changelog
    
    Co-authored-by: Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2934894153

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Added
 - My Jetpack includes JITMs
+- Use ThemeProvider when rendering Starter Plugin AdminPage
 
 ### Changed
 - E2E tests: bump dependencies
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '80d58068cf6bc873ad9f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '55ff4e481039efc63bfb');
