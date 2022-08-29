853ae28c166c82983df4c9cf93e7b198d2481dc0
dereksmart <dereksmart@users.noreply.github.com>
2022-08-29 15:35:36 +0000

    Add JITMs to Jetpack Social and starter plugin (#25880)
    * Add JITMs to Jetpack Social
    
    * changelog
    
    * Add JITMs to starter plugin
    
    * Add changes for starter plugin
    
    * Improve spacing on Social JITM
    
    * fixup versions
    
    Co-authored-by: Derek Smart <smart@automattic.com>
    Co-authored-by: Derek Smart <dsmart@dsmart-mbp.lan>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2949703989

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '55ff4e481039efc63bfb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'addaee58ce7b700cdcde');
