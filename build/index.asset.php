622cb261c7eed4919c971d8a79240082a925a59a
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2022-09-06 19:32:56 +0000

    Connection: handle connection errors in the package state (#25934)
    * Add: React vars for connection errors
    
    These changes are part of a larger project to implement consistent
    connection error presentation across monorepo projects. This portion
    focuses on providing the React variable foundation that further work
    will build upon.
    
    * changelog
    
    * Fixed project versions
    
    * Fixed GitHub checks linting errors
    
    Co-authored-by: Sergey Mitroshin <sergeymitr@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3002764785

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'addaee58ce7b700cdcde');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '9965a1f1fc1bbc4c23aa');
