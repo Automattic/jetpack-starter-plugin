45d54dcd6cf820b8cf6c8203a3d1cccd7c0b3d84
CodeyGuyDylan <CodeyGuyDylan@users.noreply.github.com>
2022-09-26 19:04:26 +0000

    Update assistant recommendation site type question (#26302)
    * [not verified] Update site type questions and update UI
    
    * [not verified] Remove unused function getSiteTypeDisplayName
    
    * [not verified] changelog
    
    * [not verified] Update areas with old names of site types
    
    * [not verified] Rename site-type-business to site-type-agency and remove site-type-other
    
    * [not verified] Improve UI for selecting checkboxes by making the entire box clickable
    
    * [not verified] Fix sizing issue with checkbox on mobile
    
    * [not verified] Add builders recommendation
    
    * [not verified] Fix test related to recommendations
    
    The new agencies recommendation comes before woocommerce, so the
    test had to be edited to expect that one first
    
    * [not verified] rename new recommendation to agency
    
    * [not verified] Change illustration used on agency recommendation
    
    * [not verified] Update discount percentage
    
    * [not verified] Rename mapStateToSummaryResourceProps to reflect change
    
    * Filter out old slugs
    
    * Fix logic error in site type filter
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3130067915

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ea101ccfb1566cf9fd5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8d232d07643b29b1e258');
