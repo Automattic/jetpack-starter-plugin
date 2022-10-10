41b56970160150ffc4f8ac7bdf02c4819993b927
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-10 21:28:18 +0000

    Search: fix CORS errors on simple sites' search dashboard (#26713)
    When I tried to create a simple site and visit the search dashboard,
    it did not work. I got CORS errors.
    
    Simple sites support for dashboard was added in #25094
    
    Before my change, the API root for JetpackRestApiClient was set to
    URL `site_url( '/wp-json/wpcom-origin/' )` from search dashboard.
    However, API has a global instance, and there are multiple
    places calling JetpackRestApiClient.setApiRoot.
    That causes the API root revert to `rest_url()` and the
    requests stop working at some point. It is overwritten with
    value from JP_CONNECTION_INITIAL_STATE instead.
    
    In this diff, I propose adding separate value `wpcomOriginApiUrl`
    to JetpackRestApiClient. For simple wpcom sites, it is set to
    `site_url( '/wp-json/wpcom-origin/' )`, for all other sites
    it's just `rest_url()` as always.
    This new `wpcomOriginApiUrl` value is only used for the search
    endpoint that can be proxied through the first party wpcom jetpack API.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3222383293

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0b386376c3b1fbf9213c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'da1e668192faf721dde9');
