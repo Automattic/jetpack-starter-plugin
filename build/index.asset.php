d302dc7f9da99fca5b990b8f97bb21e98604e4e9
anomiex <anomiex@users.noreply.github.com>
2022-10-12 18:02:35 +0000

    Lock file maintenance (#26716)
    * Lock file maintenance
    * Update snapshots for i18n-check-webpack-plugin, babel-plugin-replace-textdomain
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3236745760

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'da1e668192faf721dde9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '591bd74a23cf8f862b84');
