0feb93a97838c92df0cfdccb16e3ad4d4527190c
oskosk <oskosk@users.noreply.github.com>
2022-10-20 22:55:36 +0000

    Connection: Check if user is connected before attempting to initialize analytics with a user in useProductCheckoutWorkflow hook (#26976)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3293431263

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '173c7cf9ce9844034ea3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '55d5a71f75a3f5779958');
