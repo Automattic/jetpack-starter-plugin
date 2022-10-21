d4b032fad2523f964ac66023ce2e1e13c6564ea4
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-21 02:13:24 +0000

    Revert "Add tracks events for purchasing items in wp-admin #26964" (#26979)
    * Revert "Connection: Check if user is connected before attempting to initialize analytics with a user in useProductCheckoutWorkflow hook (#26976)"
    
    This reverts commit 11af0a3407cc25e849633a764e16917e933815be.
    
    * Revert "Add tracks events for purchasing items in wp-admin (#26964)"
    
    This reverts commit db995dd55249f063c91c7801977260ab58ba3c10.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3294248058

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '55d5a71f75a3f5779958');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '591bd74a23cf8f862b84');
