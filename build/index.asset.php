63168fbe987da03e89ddab3944826acf4bd4e32a
monsieur-z <monsieur-z@users.noreply.github.com>
2022-12-13 22:23:23 +0000

    Jetpack Backup: update logos (#27802)
    * Jetpack Backup: update logo
    
    * Add changelog
    
    * changelog
    
    * changelog
    
    * changelog
    
    * Fix packages versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3689708296

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '042fa36a275cc1e8e64d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '015ed85b434e1a932da3');
