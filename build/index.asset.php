5f3c14a413f4e137baabef3ed32486f496e3ed61
renatoagds <renatoagds@users.noreply.github.com>
2023-01-20 14:08:49 +0000

    Components: Fix responsive across elements with box-sizing removal (#28489)
    * My Jetpack: Remove local box-sizing
    
    * Components: Add box-sizing at ThemeProvider
    
    * changelog
    
    * Bump version
    
    * Components: Rename class from theme to global
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3968237065

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ac1b1e3c90848d77bb2b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '59cb99621f5e8a1f7b8e');
