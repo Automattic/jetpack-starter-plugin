31c42d301965fb3292888b24eb78ea8badcca30e
anomiex <anomiex@users.noreply.github.com>
2023-02-08 18:28:59 +0000

    Update to React 18 (#28710)
    Updates react packages and corresponding `@types/`, all the
    `@wordpress/*` and `@types/wordpress__*`, and `@testing-library/react`.
    
    Also updates some indirect deps of `@storybook/addon-actions` and
    `@storybook/react` that we have new overrides for.
    
    Also `@testing-library/react-hooks` has been merged into
    `@testing-library/react` now, so we need to remove that (and then update
    tests to get `renderHook` from the latter package instead).
    
    Additional code changes:
    
    * Replace deprecated `react-dom` methods with `@wordpress/element`
    * Adjust jest configs.
    * Add missing `act()` and other waiting in tests.
    * Declare `children` in some TypeScript types.
    * Replace deprecated wp.components.Dropdown position prop.
    * Add mocking for GB color settings.
    
    Then add new overrides to .pnpmfile.cjs. Various indirect deps don't
    declare support for React 18 yet.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4127049020

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@ This is an alpha version! The changes listed here are not final.
 - Compatibility: WordPress 6.1 compatibility
 - E2E tests: bump dependencies
 - Update playwright dependency
+- Update to React 18.
 
 ### Removed
 - E2E tests: removed deprecated Slack notification code
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '59cb99621f5e8a1f7b8e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '72cd0db599fb637d7b86');
