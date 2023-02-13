e99a81bbdb56c20c1c34bfb007f27d582a3c5178
bogiii <bogiii@users.noreply.github.com>
2023-02-13 09:55:35 +0000

    Migration plugin: Create/integrate migration progress screen (#28807)
    * Install @automattic/jetpack-api dependency
    
    * Add fetchMigrationStatus API method
    
    * Create a migration progress screen
    
    * Create a hook for fetching status in an interval
    
    * Add the term of service above the Get Started button
    
    * Introduce the MigrationLoading component
    
    * Handle 'Check your migration progress' redirection
    
    * Handle errors and show the initial migration screen
    
    ---------
    
    Co-authored-by: Wayne Wang <iverson0217@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4162032559

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '72cd0db599fb637d7b86');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1265ad4b0bce4d7b4c18');
