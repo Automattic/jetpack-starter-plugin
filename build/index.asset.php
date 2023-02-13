5e503bca2fccc3f7be346d71da89529500ef03ce
anomiex <anomiex@users.noreply.github.com>
2023-02-13 21:04:51 +0000

    Lock file maintenance (#28910)
    * Lock file maintenance
    * Update for changed jest-mock `mockReset()` behavior
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4167666522

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1265ad4b0bce4d7b4c18');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7895b54ad1bd8498f062');
