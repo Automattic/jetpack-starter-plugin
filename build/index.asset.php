e6859b902d824bf67c71460faa2604a765289b6e
jeherve <jeherve@users.noreply.github.com>
2023-02-20 18:30:08 +0000

    General: add External link icon to external links (#28922)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4226075163

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7895b54ad1bd8498f062');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f8bba0fc8685d9498ba2');
