eb05044f1ff2abdc8e79a79d0b73b66b12d03008
oskosk <oskosk@users.noreply.github.com>
2023-03-15 23:32:42 +0000

    Connection js-package: Add some debug statements (#29343)
    Co-authored-by: Sergey Mitroshin <sergeymitr@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4431812836

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '68a235fbf035153ba661');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3fdc99673c83e7de5246');
