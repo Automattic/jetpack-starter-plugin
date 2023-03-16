1b6eedd5ca9847641b8aa29d9c304966cfa94e8e
nateweller <nateweller@users.noreply.github.com>
2023-03-16 21:41:34 +0000

    Jetpack: Use the WAF REST API to manage firewall settings (#29299)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4441757382

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3fdc99673c83e7de5246');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1ac75c26794d86718002');
