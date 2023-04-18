b21c16ad6d770e2b79e555dc943c0244b8d5af6e
anomiex <anomiex@users.noreply.github.com>
2023-04-18 17:45:54 +0000

    Update storybook monorepo to v7 (major) (#30015)
    The main things to note here are:
    
    * All story files need to be named like "index.stories.ext"
    
      Storybook v7 by default won't recognize story files that aren't named
      like `.story.js` or `.stories.js`, plus if you reconfigure it to
      recognize other names it will choke on any non-story files (e.g.
      `*.d.ts`, `utils.js`, `mock-data.js`, and so on).
    
    * Stories defined in MDX are deprecated, don't write them anymore.
    
    * Controls with options (select, radio, etc) cannot be like
      `{ control: { type: 'select', options: [] } }` anymore. Instead the `options`
      needs to be one level up, like `{ control: { type: 'select' }, options: [] }`.
    
    * When using mdx for the docs, use `<Meta of={}>` and name like `index.mdx`
       to match the correponding `index.stories.ext` file.
    
      This helps sources show up right for `<Canvas>` and makes it do the
      "Docs" properly in the sidebar.
    
    * The "show code" option no longer works with a story function declared
      like
      ```
      export const () => <...>
      ```
      It requires that an `args` is passed, like
      ```
      export const args => <...>
      ```
      (note the args don't have to actually be used).
    
      Alternatively we can set `.parameters.docs.source.code` to `null` for
      the story, which disables the "show code" option entirely.
    
      Also, reassigning `args.foo` inside the render function breaks it, so don't
      do that either.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4735232211

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0ad2cf81421ae1fb92dc');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '148c422cc94592b6db61');
