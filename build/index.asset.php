626cf0d23a8d6fe95cac93f360104299f3853145
anomiex <anomiex@users.noreply.github.com>
2023-04-21 19:02:07 +0000

    Update dependency css-minimizer-webpack-plugin to v5 (#30246)
    * Update dependency css-minimizer-webpack-plugin to v5
    * Override `rollup-plugin-postcss` dep on `cssnano` so we don't have multiple
      versions of it and its deps. Looks like that package may be abandoned...
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4767867279

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '148c422cc94592b6db61');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '632c3a172b7f3ac879ac');
