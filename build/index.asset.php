652cc82b18c09a06a90fe57c4b635bbd27485994
dhasilva <dhasilva@users.noreply.github.com>
2023-04-21 19:40:47 +0000

    RNA: Fix button box-shadow radius on Linux build of Firefox (#30249)
    * fix button box-shadow-radius on firefox
    
    * changelog
    
    * bump version
    
    * adding comment about subpixel rendering issue
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4768158897

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '632c3a172b7f3ac879ac');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bcbc83513f6676b99f09');
