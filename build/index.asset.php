11e5991f31a0cd5ecef2556be29226668da5c4a8
CodeyGuyDylan <CodeyGuyDylan@users.noreply.github.com>
2023-05-17 15:02:15 +0000

    Update/boost score bar to react component (#30037)
    * [not verified] Add new component for boost score bar
    
    * [not verified] Fix component CSS
    
    * [not verified] Fix gridicons
    
    * [not verified] Add tests
    
    * [not verified] changelog
    
    * [not verified] Add a couple more tests
    
    * [not verified] Update story filename
    
    * [not verified] Update package version
    
    * [not verified] Fix storybook component
    
    * [not verified] Update colors to use variables
    
    * [not verified] Changelog
    
    * Update package versions
    
    * fix test
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5004601884

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'aee20e684c52d376a6f3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fd0ade3c03e7d7deac55');
