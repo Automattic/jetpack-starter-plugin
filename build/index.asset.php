4d846aed133d8cd66115a49ef151a8eb119466aa
sergeymitr <sergeymitr@users.noreply.github.com>
2023-05-19 21:33:30 +0000

    Connection: improve Offline Mode handling (#30570)
    * Add Offline Mode flag into the Site Health debug info
    * Action Button: Add "Disabled" prop
    * Connect Screen: Offline Mode for Required Plan component
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5028316151

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fd0ade3c03e7d7deac55');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0f4fd9ba52ac1be6b74f');
