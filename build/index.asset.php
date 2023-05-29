c02cec61ffa20ceef625279344103cff478abd88
pablinos <pablinos@users.noreply.github.com>
2023-05-29 15:38:33 +0000

    Social Previews: Fix comments from #29803 review (#31023)
    * Social Previews: Fix comments from #29803 review
    
    These fixes the three comments made by @jeherve on #29803
    
    - Adds the highlight colour to the root-variables and updates the CSS to
      the jp- vars.
    - Makes the job title text for the LinkedIn preview translatable
    - Updates the default profile image for LinkedIn to use a data URI
    
    * Update the base-styles package version.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5113289043

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0f4fd9ba52ac1be6b74f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '60b948719bb86927416d');
