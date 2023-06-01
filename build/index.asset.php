26cf499d9a76d6a48d0a640267ee99277f6a2e7e
anomiex <anomiex@users.noreply.github.com>
2023-06-01 21:26:18 +0000

    Update wordpress monorepo (#31129)
    * Update wordpress monorepo
    * Work around what seems to be a typescript bug.
      What I was seeing sounds like https://href.li/?https://github.com/microsoft/TypeScript/issues/43081
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5149204632

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '60b948719bb86927416d');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8e42fd212559179eee54');
