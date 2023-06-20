2d54308f0f72070a8e80090116ed2948d353892a
anomiex <anomiex@users.noreply.github.com>
2023-06-20 19:33:51 +0000

    Update wordpress monorepo (#31468)
    * Update wordpress monorepo
    * Remove boost `rollup-plugin-node-globals` dep.
      It appears to be abandoned and includes an obsolete acorn dep
      that chokes on `?.`, and in testing it seems that removing it makes
      no difference.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5326488467

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4778a5146bdb1533429f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9efb6b01468214cd252f');
