16a5993a2585384c355d6d113b3328cdf86bdf4b
kraftbj <kraftbj@users.noreply.github.com>
2023-06-28 21:52:47 +0000

    Jetpack Footer: fix accessibility issues (#31417)
    *  Fix accessibility issues in Jetpack footer
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5406035027

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9efb6b01468214cd252f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1fbc5691af95b86433c0');
