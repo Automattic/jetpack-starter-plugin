97b5b8eb724ae8bdb3d5a6a12cb80f4cea69b386
jeherve <jeherve@users.noreply.github.com>
2023-09-05 13:47:28 +0000

    General: remove WP 6.1 backwards compatibility checks (#32772)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6085230009

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Changed
 - General: indicate full compatibility with the latest version of WordPress, 6.3.
+- General: remove WP 6.1 backwards compatibility checks
 - General: update WordPress version requirements to WordPress 6.2.
 - Use the new method to render Connection initial state.
 
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3cbe25b90916e6d58cc4');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b715b0745bbbb6010e16');
