1824b2e65e18c8741398bb2c84acc007983373a4
anomiex <anomiex@users.noreply.github.com>
2023-10-09 22:04:14 +0000

    Update wordpress monorepo (#33428)
    * Update wordpress monorepo.
    * But let's do `@wordpress/eslint-plugin` separately again.
    * Add a pnpmfile hack for `@wordpress/icons` → `react`.
    * Remove obsolete `@wordpress/patterns` pnpmfile hack.
    * js-packages/components: Expect a warning for now. Can't fix until we drop support for WP 6.3.
    * Jetpack: Adjust test for still more role changes in color picker.
    * Jetpack: Use `within()` in tests to avoid new aria elements added to the document.
    * Jetpack: Google Calendar block: `<Placeholder instructions>` must be a string, elements are not allowed.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6461983153

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ccf92d396d8ad4b52cdf');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0a5c067b2a0b7d29e56f');
