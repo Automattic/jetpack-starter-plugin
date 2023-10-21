541f5b43b60d0d733b804c269b8dc776f0413294
elliottprogrammer <elliottprogrammer@users.noreply.github.com>
2023-10-21 01:08:59 +0000

    My Jetpack: Update checkout flow to connect "after" checkout vs before. (#33257)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6594136201

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0a5c067b2a0b7d29e56f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b95831ba7cf8b985bc86');
