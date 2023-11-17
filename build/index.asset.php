5b4ede875b316d44498dce918d99e53d3e215ad2
dhasilva <dhasilva@users.noreply.github.com>
2023-11-17 15:22:02 +0000

    My Jetpack: Add direct checkout support for products with quantity-based plans (#34177)
    * add quantity to product checkout workflow hook and product detail card
    
    * pass quantity from jetpack ai to product interstitial
    
    * simplify click handler
    
    * add direct checkout for products with usage tiers
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6905698044

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8a987df178e6a03b8b2c');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'afc76dcb6f73c36f7ea0');
