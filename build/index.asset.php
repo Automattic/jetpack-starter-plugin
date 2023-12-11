7ed26fa91629135cad95e79a1d002d67a90a356e
anomiex <anomiex@users.noreply.github.com>
2023-12-11 21:18:26 +0000

    Lock file maintenance (#34559)
    * Lock file maintenance
    * Remove obsolete pnpmfile hacks
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7173325842

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'afc76dcb6f73c36f7ea0');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cce9f88639e650777845');
