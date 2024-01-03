43a468aa1706fb7efe1d6dd3608f397f314329d0
darssen <darssen@users.noreply.github.com>
2024-01-03 18:44:37 +0000

    Fix/jetpack footer inconsistent behavior (#34787)
    * About link shows now as not external in admin-links scenario
    
    * Added siteadminUrl for AdminPage component to pass it to JetpackFooter
    
    * Added getSiteAdminUrl tool in components.
    
    * Refactored JetpackFooter and its usages to use getSiteAdminUrl from tools instead from props.
    
    * Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7401056719

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6b94998a15bc30e6fd2d');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8a5b93d7468c4ef90806');
