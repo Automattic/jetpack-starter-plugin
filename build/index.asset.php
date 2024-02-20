7554914b264c07900875aec19da69d62bc5ba0b0
andrii-lysenko <andrii-lysenko@users.noreply.github.com>
2024-02-20 20:39:45 +0000

    Connection/external links accessibility (#35733)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7979578088

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9a6b989a0986c066d6b3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ba8922dc553d9aabb363');
