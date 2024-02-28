b8c94d3607265a604aa4a998bb5a375893e0a8a7
Initsogar <Initsogar@users.noreply.github.com>
2024-02-28 20:27:53 +0000

    Backup: Add backup preflight endpoint in backup package and js-package (#36032)
    * Add rewind/preflight endpoint on backup package
    
    * Map backup/preflight API on js-packages API
    
    * Add fetchBackupPreflightStatus test
    
    * changelog
    
    * Update jetpack-api package version
    
    * Return more error details
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8086480752

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '51ad6811344991f84abc');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9f72956a0068590d8c95');
