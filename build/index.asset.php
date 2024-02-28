9dddf09e2f9cc56517b6bbedb530f7ff03e93c9a
jboland88 <jboland88@users.noreply.github.com>
2024-02-28 20:30:25 +0000

    Improve My Jetpack checkout flow consistency (#35908)
    * Update checkout flows from product interstitial pages to be more consistent, always install standalone and redirect to plugin page
    
    * changelog
    
    * Allow post-purchse redirect to be updated after product activation
    
    * changelog
    
    * Move placement of post_checkout_url function for backup
    
    * version management
    
    * Actually return the post purchase URL for backups in My Jetpack
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8086537220

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9f72956a0068590d8c95');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '66c4d0460641e8b10982');
