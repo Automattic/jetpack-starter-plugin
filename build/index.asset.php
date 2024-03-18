8df2e3042d33f05424dcd94a4a1c6334178eb38b
jboland88 <jboland88@users.noreply.github.com>
2024-03-18 21:53:41 +0000

    Fix/weirdness with post register redirection on purchase (#36448)
    * Allow the redirect url to be passed after site registration in the checkout workflow
    
    * changelog
    
    * Version management
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8334003451

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c9e2b01ed38f8df85cc7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9426c4fa25c0481718e5');
