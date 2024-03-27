4d50a51497f012bc500ac1f14461c8521c3faebf
jboland88 <jboland88@users.noreply.github.com>
2024-03-27 19:03:50 +0000

    Fix/my jetpack logo spacing (#36609)
    * Prevent margin collapse in mian page container on My Jetpack
    
    * changelog
    
    * Version bump
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8457091725

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '96ef3482b75b3472fa30');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fc3e20b791fdfb8f06cb');
