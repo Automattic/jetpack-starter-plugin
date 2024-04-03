78dc30e74fb667e609ff3e91a599319d83dd91d7
nateweller <nateweller@users.noreply.github.com>
2024-04-03 23:34:20 +0000

    A4A: Add connection card and related components (#36664)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8546789543

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fc3e20b791fdfb8f06cb');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '68291a2a1577680dd015');
