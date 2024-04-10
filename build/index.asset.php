f1009892162e1e733dc3f453ab9c82a7a9cf268f
IanRamosC <IanRamosC@users.noreply.github.com>
2024-04-10 15:04:17 +0000

    My Jetpack: Add async site connection to site connection Notice (#36771)
    * Add async connection to notice
    
    * changelog
    
    * Adjust notice style
    
    * changelog
    
    * Fix Notice actions
    
    * Add loadingText to action button
    
    * Delete unused file
    
    * Add 'from' property to connection hook
    
    * Update changelog versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8633305768

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0ad183482d41e2febbe3');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ad2ba70343a9014981b');
