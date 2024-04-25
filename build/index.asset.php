312346b34453eb916a554bf59a4c7aa12bd8dbcf
arthur791004 <arthur791004@users.noreply.github.com>
2024-04-25 01:57:03 +0000

    Base Styles: Reduce specificity of body selector (#37043)
    * Try reducing specificity of the body selector
    
    * changelog
    
    * Bump version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8825744541
    
    Upstream-Ref: Automattic/jetpack@61f96da2b08ccdf2b67dcbaeb5358d75993f4589

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ad2ba70343a9014981b');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '54632f7708718d218fbb');
