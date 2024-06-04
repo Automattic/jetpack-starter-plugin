2c3fa76c02d6ec767404e9c5ac65f5ae518f7942
anomiex <anomiex@users.noreply.github.com>
2024-06-04 16:59:59 +0000

    Update wordpress monorepo (#37669)
    * Update wordpress monorepo
    * Pnpmfile hack for `@wordpress/primitives` missing `react` dep
    * jest: Mock `client-zip`, it doesn't even try to work with jest.
    * Update tests for wordpress/gutenberg#61366
    * Don't upgrade `@wordpress/dependency-extraction-webpack-plugin` past 5.9.0
    * Add pnpmfile hack to prevent accidentally re-updating `@wordpress/dependency-extraction-webpack-plugin`.
    * Update `@automattic/social-previews` pnpmfile hack to avoid duplicate `@wordpress/*` package versions.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9370984130
    
    Upstream-Ref: Automattic/jetpack@3273e03932bb8d55d574ae540493b536b1bdbf2b

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3e9a3271918e4dd37c70');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '01f103860715cff13055');
