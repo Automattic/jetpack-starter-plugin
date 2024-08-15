39e0d0d99e19c0e0e87ea051370c29046c30f6d3
anomiex <anomiex@users.noreply.github.com>
2024-08-15 20:36:35 +0000

    Lock file maintenance (#38822)
    * Lock file maintenance
    * Phan: Suppress PhanDeprecatedImplicitNullableParam when supporting PHP 7.0
    * Fix new Phan issues
      * PhanDeprecatedImplicitNullableParam in plugins/crm
      * Calls passing string to `update_option()` `$autoload`, which in WP 6.6
        was changed to bool (but silently still accepting string for back
        compat).
    * Update phan baselines
    * Phan: WP 6.6 fixed a phpdoc which removes something from the baseline, but we still need a suppression for the WP 6.5 compat run.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10410008930
    
    Upstream-Ref: Automattic/jetpack@cf8996c291630bd8f0feef8ecce6c6f8e7976e16

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '891e409de727d446bc28');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2034d910955a2ff5c6e2');
