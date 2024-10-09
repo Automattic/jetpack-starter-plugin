e5068d95042953e5658fc6ca324aed7d7ab44d65
anomiex <anomiex@users.noreply.github.com>
2024-10-09 20:02:04 +0000

    eslint: Enable more react rules (#39421)
    * `react-hooks/exhaustive-deps`: Apply to `useSelect`.
    * `react/no-direct-mutation-state`
    * `react/no-find-dom-node`
    * `react/no-unescaped-entities`
    
    The first of those accounts for the majority of fixes in this PR, we had
    a bunch of places either missing a dependency or specifying a dependency
    unnecessarily. Of particular note is that constants defined outside of
    the `render()` function, like `STORE_ID`, don't need to be dependencies.
    
    Of the rest, `react/no-direct-mutation-state` was only violated in one
    place, `react/no-unescaped-entities` in one storybook story, and
    `react/no-find-dom-node` was already fixed everywhere.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11261999141
    
    Upstream-Ref: Automattic/jetpack@f2c8bd39b027427c282cb3e03cdeb8fe8dfdc2f7

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '67d482375aa3e05b7069');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a34c3d2fd32f6b3dc567');
