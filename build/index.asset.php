a6f92712d95f247598f1dfe39e53b5969c65ce1d
simison <simison@users.noreply.github.com>
2026-04-03 15:40:18 +0000

    Boost: replace footer (#47921)
    * Boost: replace footer
    * JetpackFooter component: Pin base font-size to prevent page context leakage
    
    ---------
    
    Co-authored-by: Christian Gastrell <cgastrell@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23951433883
    
    Upstream-Ref: Automattic/jetpack@5781fc36bc16e0cd15f361b9844099604ff8f550

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '71feee12c53013335f38');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'd8488391a65ee7809639');
