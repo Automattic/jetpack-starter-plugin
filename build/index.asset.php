9e6f36d4d629671f9a36999e57dc7870e84bfbe1
anomiex <anomiex@users.noreply.github.com>
2026-04-06 20:30:31 +0000

    Update wordpress monorepo (#47890)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24049007940
    
    Upstream-Ref: Automattic/jetpack@b32a03d4a9aac587fef05f723b334abeff68874f

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'fef1f019da17aafdcace');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'ee5257faff40bff4e1b8');
