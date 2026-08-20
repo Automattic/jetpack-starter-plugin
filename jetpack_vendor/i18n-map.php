66e14f052d9693bf9ab3e1e1d32647cfcefb4678
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-08-20 21:13:42 +0000

    Backport jetpack 16.2-a.1 changes (#51438)
    * Changelog and readme.txt edits.
    
    * Sort changelog entries.
    
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32416491751
    
    Upstream-Ref: Automattic/jetpack@3f6d67ce2678187fc41031f44807064a9279746e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.11.0-alpha1787245368',
+      'ver' => '8.11.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.44.0-alpha1787245368',
+      'ver' => '5.44.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
